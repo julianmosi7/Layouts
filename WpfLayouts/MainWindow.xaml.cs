6ac2f488cfb112c6d4d9c67b449ef158b216c252
julianmosi7 <moshammer.ju@gmail.com>
2019-10-15 19:09:52 +0200

    almost ready

--- WpfLayouts/MainWindow.xaml.cs
+++ WpfLayouts/MainWindow.xaml.cs
@@ -24,7 +24,26 @@ namespace WpfLayouts
        
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-           //StackPanel.Children.Add{ new Button { }}
+            Button buttonBackward = new Button();
+            buttonBackward.Content = new Image().Source = new BitmapImage(new Uri("backward.png", UriKind.Relative));
+            StackPanel.Children.Add(buttonBackward);
+
+            Button buttonForward = new Button();
+            buttonForward.Content = new Image().Source = new BitmapImage(new Uri("forward.png", UriKind.Relative));
+            StackPanel.Children.Add(buttonForward);
+
+            Button buttonOpen = new Button();
+            buttonOpen.Content = new Image().Source = new BitmapImage(new Uri("open.png", UriKind.Relative));
+            StackPanel.Children.Add(buttonOpen);
+
+            Button buttonSave = new Button();
+            buttonSave.Content = new Image().Source = new BitmapImage(new Uri("save.png", UriKind.Relative));
+            StackPanel.Children.Add(buttonSave);
+
+            Button buttonSaveAll = new Button();
+            buttonSaveAll.Content = new Image().Source = new BitmapImage(new Uri("save_all.png", UriKind.Relative));
+            StackPanel.Children.Add(buttonSaveAll);
         }
     }
 }
+ 
